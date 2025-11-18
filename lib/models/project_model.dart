4d1615aba8235c03d74979a41f900924271c9f5b
Shamsheer <shamsheersha567@gmail.com>
2025-11-18 21:31:44 +0530

    Added Project Model

---
+++ lib/models/project_model.dart
@@ -0,0 +1,102 @@
+class ProjectModel {
+
+  final String id;
+  final String title;
+  final String subject;
+  final String author;
+  final String imagePath;
+  final String grade;
+
+  ProjectModel({
+    required this.id,
+    required this.title,
+    required this.subject,
+    required this.author,
+    required this.imagePath,
+    required this.grade,
+  });
+
+
+  List projectList = [
+  ProjectModel(
+    id: '1',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+  ProjectModel(
+    id: '2',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+  ProjectModel(
+    id: '3',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+  ProjectModel(
+    id: '4',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+  ProjectModel(
+    id: '5',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+  ProjectModel(
+    id: '6',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+  ProjectModel(
+    id: '7',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+  ProjectModel(
+    id: '8',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+  ProjectModel(
+    id: '9',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+  ProjectModel(
+    id: '10',
+    title: 'Kemampuan Merangkum Tulisan',
+    subject: 'BAHASA SUNDA',
+    author: 'Oleh Al-Baiqi Samaan',
+    imagePath: 'assets/images/Rectangle 19.jpg',
+    grade: 'A',
+  ),
+];
+}
